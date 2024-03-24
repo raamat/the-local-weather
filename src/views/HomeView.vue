e2d8a16e12af86f8c7bcd36253de9e2e13540534
Raamat <raamat@bk.ru>
2024-03-24 21:58:24 +0300

    feat: add API error handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,22 +8,25 @@
         placeholder="Введите название города"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
       />
-      <ul v-if="searchResult"
+      <ul
+        v-if="searchResult"
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
       >
-        <li
-          v-for="result in searchResult"
-          :key="result.id"
-          class="py-2 cursor-pointer"
-        >
-          {{ result.name }} ({{ result.region }}), {{ result.country }}
-        </li>
+        <p v-if="searchError">Что-то пошло не так. Попробуйте ещё раз.</p>
+        <p v-if="!searchError && searchResult.length === 0">
+          Ничего не найдено
+        </p>
+        <template v-else>
+          <li
+            v-for="result in searchResult"
+            :key="result.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ result.name }} ({{ result.region }}), {{ result.country }}
+          </li>
+        </template>
       </ul>
     </div>
-    <!-- <p>{{ searchQuery }}</p>
-    <button class="bg-slate-600 px-1 py-1" @click="getWeather(searchQuery)">
-      Поиск
-    </button> -->
   </main>
 </template>
 
@@ -38,7 +41,8 @@ const API_KEY = "1f672b032d5b4f77a3f172349242003";
 
 const searchQuery = ref("");
 const queryTimeout = ref(null);
-let searchResult = ref(null);
+const searchResult = ref(null);
+const searchError = ref(null);
 
 async function getWeather(query) {
   try {
@@ -46,11 +50,9 @@ async function getWeather(query) {
       `${BASE_URL}search.json?key=${API_KEY}&q=${query}`
     );
     const city = await cityResponse.json();
-    //console.log(`Температура в городе ${query}: ${city.current.temp_c} ℃`);
-    //console.log(city)
     return city;
   } catch (err) {
-    console.error("Произошла ошибка!", err);
+    console.error("Произошла ошибка при запросе к API", err);
   }
 }
 
@@ -58,11 +60,12 @@ function getSearchResult() {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await getWeather(searchQuery.value);
-      //searchResult.value = result.data.features;
-      searchResult.value = result;
-      console.log(searchResult)
-      console.log(searchResult.value)
+      try {
+        const result = await getWeather(searchQuery.value);
+        searchResult.value = result;
+      } catch {
+        searchError.value = true;
+      }
       return;
     }
     searchResult.value = null;
